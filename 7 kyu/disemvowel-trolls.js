1bd10b11b9a895c3380f724ddd3919e574e98370
Michael Yakovlev <kvadrofilii@gmail.com>
2022-04-21 19:41:41 +0300

    feat: add disemvowel-trolls

---
+++ 7 kyu/disemvowel-trolls.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const disemvowel = (str) => {
+  // Переменная со всеми гласными буквами кроме Y
+  const vowel = 'euioaEUIOA';
+  // Преобразуем строку в массив
+  let arr = [...str];
+
+  // Перебираем гласные буквы
+  for (let i = 0; i < vowel.length; i++) {
+    // Фильтруем и удаляем совпавшие буквы
+    arr = arr.filter((item) => (item !== vowel[i]));
+  }
+
+  // Преобразуем массив в строку
+  return arr.join('');
+}
+
+disemvowel("This website is for losers LOL!");
+
+//Trolls are attacking your comment section!
+
+//A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+//Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+//For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+//Note: for this kata y isn't considered a vowel.
